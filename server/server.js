c50b6d8f9473994b9851ebd0b85974545a822810
Luke Thompson <16175583curtin@gmail.com>
2021-10-24 16:29:54 +0800

    fix server bug in v3 apollo

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const server = new ApolloServer({
   typeDefs,
@@ -20,20 +22,20 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
+server.start()
+  .then(res => {
+      server.applyMiddleware({ app });
+      db.once('open', () => {
+        app.listen(PORT, () => {
+          console.log(`API server running on port ${PORT}!`);
+          console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        });
+      });
+  })
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
